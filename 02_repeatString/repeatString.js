528456a673a960ab92e60d9c8843b3bf35f8d308
ioangheraszim <gijohny17@gmail.com>
2025-01-20 15:48:23 +0000

    Finished exercise 2
    Finished exercise 2 by using by using two parameters, str and num. I have made another variable named word and assigned it an empty string to hold the repeated version of the string, then used a for loop to loop through the number of times desired, after each iteration i is incremented by 1 and inside the loop thestring (str) is added to the variable word using += operator. Once the loop has run num times the variable word will contain the original string str repetead num times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+const repeatString = (str,num) => {
+    let word = "";
+    for (let i = 0; i < num; i++){
+        word += str;
+    }
 
-};
+    return word;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
