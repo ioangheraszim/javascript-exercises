ae19d2209b11a154103b0262185f6e28b8998434
ioangheraszim <gijohny17@gmail.com>
2025-01-20 22:05:46 +0000

    Finished Exercise 6
    Exercise 6 was done by using an if statement that returns true if the argument matches a leap year, it was also taken in cosideration that a leap year is divisible by 4 but not divisible by 100 so 1900 would not be a leap year even though it is

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = (currentYear) => {
+    if(currentYear % 4 === 0 && currentYear % 100 !== 0) {
+        return true;
+    }
 };
 
 // Do not edit below this line
