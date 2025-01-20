5389967ee13a4ead704edaf6951328e6e57262de
ioangheraszim <gijohny17@gmail.com>
2025-01-20 21:36:29 +0000

    remade exercise 5 following specification in readme file

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,16 @@
-const sumAll = function() {
-    let arr = [1, 2, 3, 4];
-    let sumOfAll = arr.reduce((total, sum) => total + sum);
-    return sumOfAll;
+const sumAll = (start, end) => {
+    
+    let total = 0;
+
+    for (let i = start; i <= end; i++){
+      total += i;
+    }
+    
+    return total;
+    
+    // let arr = [1, 2, 3, 4];
+    // let sumOfAll = arr.reduce((total, sum) => total + sum);
+    // return sumOfAll;
 };
 
 // Do not edit below this line
