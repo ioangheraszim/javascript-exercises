7a7fce76eda79c01e0dc83bda7496c5cdbd33b3d
ioangheraszim <gijohny17@gmail.com>
2025-01-24 15:23:51 +0000

    Normalize input and check for palindrome
    - Converted to lower case and removed a to z and 0 to 9 from starting
    - Reversed the normalized string
    - Compared the two strings normalise and reverseString

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let normalStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reverseStr = normalStr.split('').reverse().join('').toLowerCase();
+    
+   return normalStr === reverseStr
 };
 
 // Do not edit below this line
